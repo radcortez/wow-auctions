f8743b6f424c01b73efffc9efed2728de6aa3ae8
Roberto Cortez <radcortez@yahoo.com>
2020-08-06 15:02:05 +0100

    Use RestClient for ConnectedRealms.

---
+++ batch/src/main/java/com/radcortez/wow/auctions/api/ConnectedRealmsApi.java
@@ -0,0 +1,20 @@
+package com.radcortez.wow.auctions.api;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@RegisterRestClient
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Path("/data/wow/connected-realm")
+public interface ConnectedRealmsApi {
+    @GET
+    @Path("/index")
+    ConnectedRealms index(@QueryParam("namespace") String namespace, @QueryParam("locale") String locale);
+}
--- batch/src/main/java/com/radcortez/wow/auctions/batch/prepare/ConnectRealmsBatchlet.java
+++ batch/src/main/java/com/radcortez/wow/auctions/batch/prepare/ConnectRealmsBatchlet.java
@@ -2,16 +2,20 @@ package com.radcortez.wow.auctions.batch.prepare;
 
 import com.radcortez.wow.auctions.api.ConnectedRealm;
 import com.radcortez.wow.auctions.api.ConnectedRealms;
+import com.radcortez.wow.auctions.api.ConnectedRealmsApi;
 import com.radcortez.wow.auctions.api.Realm;
 import com.radcortez.wow.auctions.business.WoWBusinessBean;
 import com.radcortez.wow.auctions.entity.Region;
 import com.radcortez.wow.auctions.mapper.ConnectedRealmMapper;
 import lombok.extern.java.Log;
+import org.eclipse.microprofile.rest.client.RestClientBuilder;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.batch.api.AbstractBatchlet;
 import javax.batch.api.BatchProperty;
+import javax.batch.runtime.BatchStatus;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -47,38 +51,33 @@ public class ConnectRealmsBatchlet extends AbstractBatchlet {
     String region;
 
     Client client;
+    ConnectedRealmsApi connectedRealmsApi;
 
     @PostConstruct
     void init() {
-        client = ClientBuilder.newClient();
+        client = ClientBuilder.newClient().property("region", region);
+        connectedRealmsApi =
+            RestClientBuilder.newBuilder()
+                             .baseUri(UriBuilder.fromUri(host).build(region))
+                             .property("region", region)
+                             .build(ConnectedRealmsApi.class);
     }
 
     @Override
     public String process() {
         log.info(this.getClass().getSimpleName() + " running");
 
-        ConnectedRealms connectedRealms =
-            client.target(UriBuilder.fromUri(host).resolveTemplate("region", region))
-                  .path(endpoint)
-                  .queryParam("namespace", "dynamic-" + region)
-                  .queryParam("locale", locale)
-                  .request(MediaType.APPLICATION_JSON)
-                  .property("region", region)
-                  .get(ConnectedRealms.class);
-
+        ConnectedRealms connectedRealms = connectedRealmsApi.index("dynamic-" + region, locale);
         connectedRealms.getConnectedRealms().forEach(location -> createConnectedRealmFromUri(location.getHref()));
 
         log.info(this.getClass().getSimpleName() + " completed");
-        return "COMPLETED";
+        return BatchStatus.COMPLETED.toString();
     }
 
     private void createConnectedRealmFromUri(final URI connectedRealmUri) {
         final ConnectedRealm connectedRealm =
             client.target(connectedRealmUri)
-                  .queryParam("namespace", "dynamic-" + region)
-                  .queryParam("locale", locale)
                   .request(MediaType.APPLICATION_JSON)
-                  .property("region", region)
                   .get(ConnectedRealm.class);
 
         //woWBusiness.findConnectedRealm(connectedRealm.getId());
