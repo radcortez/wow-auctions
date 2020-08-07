9e483e39a85c7990ab7b996a6f221707dd5a1eb3
Roberto Cortez <radcortez@yahoo.com>
2020-08-07 01:29:15 +0100

    Add LocationApi.

---
+++ batch/src/main/java/com/radcortez/wow/auctions/api/LocationApi.java
@@ -0,0 +1,22 @@
+package com.radcortez.wow.auctions.api;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@RegisterRestClient
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public interface LocationApi {
+    @GET
+    @Path("/{location}")
+    ConnectedRealm getConnectedRealm(@PathParam ("location") String location,
+                                     @QueryParam("namespace") String namespace,
+                                     @QueryParam("locale") String locale);
+}
--- batch/src/main/java/com/radcortez/wow/auctions/batch/prepare/ConnectRealmsBatchlet.java
+++ batch/src/main/java/com/radcortez/wow/auctions/batch/prepare/ConnectRealmsBatchlet.java
@@ -1,10 +1,8 @@
 package com.radcortez.wow.auctions.batch.prepare;
 
-import com.radcortez.wow.auctions.api.ConnectedRealms;
 import com.radcortez.wow.auctions.api.ConnectedRealmsApi;
-import com.radcortez.wow.auctions.api.Status;
+import com.radcortez.wow.auctions.api.LocationApi;
 import com.radcortez.wow.auctions.entity.ConnectedRealm;
-import com.radcortez.wow.auctions.mapper.ConnectedRealmMapper;
 import lombok.extern.java.Log;
 import org.eclipse.microprofile.rest.client.RestClientBuilder;
 
@@ -19,12 +17,8 @@ import javax.inject.Named;
 import javax.transaction.Transactional;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriBuilder;
 import java.net.URI;
-import java.util.Optional;
-
-import static com.radcortez.wow.auctions.api.Status.Type.DOWN;
 
 /**
  * @author Roberto Cortez
@@ -40,23 +34,25 @@ public class ConnectRealmsBatchlet extends AbstractBatchlet {
     @BatchProperty(name = "host")
     String host;
     @Inject
-    @BatchProperty(name = "endpoint")
-    String endpoint;
-    @Inject
     @BatchProperty(name = "region")
     String region;
 
-    Client client;
     ConnectedRealmsApi connectedRealmsApi;
+    LocationApi locationApi;
 
     @PostConstruct
     void init() {
-        client = ClientBuilder.newClient().property("region", region);
         connectedRealmsApi =
             RestClientBuilder.newBuilder()
                              .baseUri(UriBuilder.fromUri(host).build(region))
                              .property("region", region)
                              .build(ConnectedRealmsApi.class);
+
+        locationApi =
+            RestClientBuilder.newBuilder()
+                             .baseUri(UriBuilder.fromUri(host).build(region))
+                             .property("region", region)
+                             .build(LocationApi.class);
     }
 
     @Override
@@ -73,12 +69,8 @@ public class ConnectRealmsBatchlet extends AbstractBatchlet {
     }
 
     private void createConnectedRealmFromUri(final URI connectedRealmUri) {
-        final com.radcortez.wow.auctions.api.ConnectedRealm connectedRealm =
-            client.target(connectedRealmUri)
-                  .queryParam("namespace", "dynamic-" + region)
-                  .queryParam("locale", locale)
-                  .request(MediaType.APPLICATION_JSON)
-                  .get(com.radcortez.wow.auctions.api.ConnectedRealm.class);
+        com.radcortez.wow.auctions.api.ConnectedRealm connectedRealm =
+            locationApi.getConnectedRealm(connectedRealmUri.getPath(), "dynamic-" + region, locale);
 
         log.info("Connected Realm " + connectedRealm);
         if (connectedRealm.isDown()) {
@@ -88,10 +80,5 @@ public class ConnectRealmsBatchlet extends AbstractBatchlet {
         ConnectedRealm connectedRealmEntity = connectedRealm.toEntity(region);
         ConnectedRealm.<ConnectedRealm>findByIdOptional(connectedRealm.getId())
             .ifPresentOrElse(connectedRealmEntity::update, connectedRealmEntity::create);
-    }
-
-    @PreDestroy
-    void destroy() {
-        client.close();
     }
 }
