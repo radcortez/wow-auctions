b9e66b2985820963737330bfdc5d45ab37996c65
Roberto Cortez <radcortez@yahoo.com>
2020-10-29 20:48:04 +0000

    Use Quarkiverse JBeret. (#7)
    Use Quarkiverse JBeret.

--- batch/src/test/java/com/radcortez/wow/auctions/QuarkusDataSourceProvider.java
+++ batch/src/test/java/com/radcortez/wow/auctions/QuarkusDataSourceProvider.java
@@ -2,12 +2,28 @@ package com.radcortez.wow.auctions;
 
 import com.radcortez.flyway.test.junit.DataSourceInfo;
 import com.radcortez.flyway.test.junit.DataSourceProvider;
-import org.eclipse.microprofile.config.ConfigProvider;
+import io.smallrye.config.PropertiesConfigSource;
+import io.smallrye.config.SmallRyeConfig;
+import io.smallrye.config.SmallRyeConfigBuilder;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.net.URL;
 
 public class QuarkusDataSourceProvider implements DataSourceProvider {
     @Override
+    @SneakyThrows
     public DataSourceInfo getDatasourceInfo(final ExtensionContext extensionContext) {
-        return DataSourceInfo.config(ConfigProvider.getConfig().getValue("quarkus.datasource.jdbc.url", String.class));
+        // We don't have access to the Quarkus CL here, so we cannot use ConfigProvider.getConfig() to retrieve the same configuration.
+
+        URL properties = Thread.currentThread().getContextClassLoader().getResource("application.properties");
+        assert properties != null;
+
+        SmallRyeConfig config = new SmallRyeConfigBuilder()
+            .withSources(new PropertiesConfigSource(properties))
+            .withProfile("test")
+            .build();
+
+        return DataSourceInfo.config(config.getRawValue("quarkus.datasource.jdbc.url"));
     }
 }
